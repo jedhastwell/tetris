8ad1b5f0488ca5889e1f958c5a5a368a781412f3
Jed Hastwell <jed@whistlefire.com>
2020-12-06 23:24:58 +0100

    Add support for testing wtih Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFiles: ['jest-canvas-mock'],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+}
