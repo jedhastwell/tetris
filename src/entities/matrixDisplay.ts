1954908411ddb40a267ac2c5433ba5832692d84d
Jed Hastwell <jed@whistlefire.com>
2020-12-08 16:15:32 +0100

    Add MatrixDisplay, a graphics game object for visualising a matrix

---
+++ src/entities/matrixDisplay.ts
@@ -0,0 +1,48 @@
+import Phaser from 'phaser'
+import { ShapeId } from '../types'
+import { Matrix } from '../gameplay/matrix'
+
+const Colors = {
+  grid: 0x4c4b4c,
+  background: 0x262626,
+  [ShapeId.I]: 0x2cade2,
+  [ShapeId.J]: 0x005a9d,
+  [ShapeId.L]: 0xf89622,
+  [ShapeId.O]: 0xfde102,
+  [ShapeId.S]: 0xee2733,
+  [ShapeId.Z]: 0x4eb748,
+  [ShapeId.T]: 0x922b8c,
+}
+
+class MatrixDisplay extends Phaser.GameObjects.Graphics {
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    public cellWidth: number,
+    public cellHeight: number,
+    public gridStroke: number,
+  ) {
+    super(scene, { x, y })
+  }
+
+  draw(matrix: Matrix): void {
+    this.clear()
+    this.lineStyle(this.gridStroke, Colors.grid)
+    this.fillStyle(Colors.background)
+    this.fillRect(0, 0, matrix.length * this.cellWidth, matrix[0].length * this.cellHeight)
+    this.strokeRect(0, 0, matrix.length * this.cellWidth, matrix[0].length * this.cellHeight)
+    matrix.forEach((row, r) => {
+      row.forEach((value, c) => {
+        if (value === 0) {
+          this.strokeRect(c * this.cellWidth, r * this.cellHeight, this.cellWidth, this.cellHeight)
+        } else {
+          this.fillStyle(Colors[value])
+          this.fillRect(c * this.cellWidth, r * this.cellHeight, this.cellWidth, this.cellHeight)
+        }
+      })
+    })
+  }
+}
+
+export default MatrixDisplay
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,16 +1,27 @@
 import Phaser from 'phaser'
+import MatrixDisplay from '../entities/matrixDisplay'
+import Tetromino from '../gameplay/tetromino'
+import { ShapeId } from '../types'
 
 class Game extends Phaser.Scene {
+  private debugMatrix: MatrixDisplay
+
   constructor() {
     super({ key: 'GameScene' })
   }
 
-  preload(): void {
-    this.load.image('robotune', 'assets/robotune.png')
-  }
+  preload(): void {}
 
   create(): void {
-    this.add.image(400, 300, 'robotune')
+    this.debugMatrix = new MatrixDisplay(this, 10, 10, 40, 40, 1)
+    this.add.existing(this.debugMatrix)
+
+    let shape = ShapeId.I
+    this.debugMatrix.draw(Tetromino.getMatrix(shape, 0))
+    this.input.on('pointerdown', () => {
+      shape = (shape % ShapeId.T) + 1
+      this.debugMatrix.draw(Tetromino.getMatrix(shape, 0))
+    })
   }
 
   update(time: number, delta: number): void {}
