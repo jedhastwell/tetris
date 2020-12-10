7cedde8d4d0c5813b51329fdb58da55fd17cda69
Jed Hastwell <jed@whistlefire.com>
2020-12-10 14:40:44 +0100

    Update Game scene for initial testing of playfield

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,10 +1,11 @@
 import Phaser from 'phaser'
 import MatrixDisplay from '../entities/matrixDisplay'
 import Tetromino from '../gameplay/tetromino'
-import { ShapeId } from '../types'
+import Playfield from '../gameplay/playfield'
 
 class Game extends Phaser.Scene {
   private debugMatrix: MatrixDisplay
+  private playfield: Playfield
 
   constructor() {
     super({ key: 'GameScene' })
@@ -13,18 +14,29 @@ class Game extends Phaser.Scene {
   preload(): void {}
 
   create(): void {
-    this.debugMatrix = new MatrixDisplay(this, 10, 10, 40, 40, 1)
+    this.debugMatrix = new MatrixDisplay(this, 10, 10, 20, 20, 1)
     this.add.existing(this.debugMatrix)
 
-    let shape = ShapeId.I
-    this.debugMatrix.draw(Tetromino.getMatrix(shape, 0))
-    this.input.on('pointerdown', () => {
-      shape = (shape % ShapeId.T) + 1
-      this.debugMatrix.draw(Tetromino.getMatrix(shape, 0))
+    this.playfield = new Playfield({
+      cols: 10,
+      rows: 28,
+      firstVisibleRow: 8,
+    })
+
+    const keys = ['LEFT', 'RIGHT', 'DOWN']
+    keys.forEach((key) => {
+      this.input.keyboard
+        .addKey(key, true)
+        .on('down', () => this.playfield.tryMove((<any>Tetromino.Moves)[key]))
     })
   }
 
-  update(time: number, delta: number): void {}
+  update(time: number, delta: number): void {
+    this.playfield.update(delta)
+    const output = this.playfield.getMatrix(true)
+    output.splice(0, 8)
+    this.debugMatrix.draw(output)
+  }
 }
 
 export default Game
