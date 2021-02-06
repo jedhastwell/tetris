fec96c4c7608e93df05658a5c26904bb09fd2cc2
Jed Hastwell <jed@whistlefire.com>
2021-02-06 17:58:45 +0100

    Add animation to update of preview panels

--- src/entities/preview.ts
+++ src/entities/preview.ts
@@ -8,6 +8,7 @@ const SHAPE_IMAGES = '-IJLOSZT'
 
 class PreviewPanel extends Phaser.GameObjects.Container {
   tetrominos: Phaser.GameObjects.Group
+  rotationOffset: number
 
   constructor(
     scene: Phaser.Scene,
@@ -37,6 +38,8 @@ class PreviewPanel extends Phaser.GameObjects.Container {
     this.add(footer)
     this.add(title)
 
+    this.rotationOffset = 0
+
     this.tetrominos = this.scene.add.group(<Phaser.Types.GameObjects.Group.GroupCreateConfig>{
       key: 'atlas',
       frame: 'tetromino-O',
@@ -49,14 +52,50 @@ class PreviewPanel extends Phaser.GameObjects.Container {
   }
 
   updateShapes(value: ShapeId | ShapeId[] | null): void {
+    const initialSlotY = HEADER_BOTTOM + (BLOCK_HEIGHT + SLOT_HEIGHT) * 0.5
+    const duration = 200
     const tetrominos = <Phaser.GameObjects.Image[]>this.tetrominos.getChildren()
     const shapes = Array.isArray(value) ? value : [value]
+    const images = tetrominos.map(
+      (t, i) => tetrominos[(i + this.rotationOffset) % tetrominos.length],
+    )
+    this.rotationOffset = (this.rotationOffset + 1) % tetrominos.length
 
-    tetrominos.forEach((image, i) => {
-      const shapeId = shapes[i]
-      image.setVisible(!!shapeId)
+    images.forEach((image, i) => {
+      const shapeId = shapes[i - 1]
       if (!!shapeId) {
         image.setFrame(`tetromino-${SHAPE_IMAGES[shapeId]}`)
+      }
+
+      if (i === 0) {
+        image.scale = 1
+        this.scene.tweens.add({
+          targets: image,
+          duration: duration,
+          scale: 0,
+          alpha: 0,
+          ease: Phaser.Math.Easing.Cubic.In,
+        })
+      } else if (i === images.length - 1) {
+        image.y = initialSlotY + SLOT_HEIGHT * (i - 1)
+        image.setVisible(!!shapeId)
+        image.setScale(0)
+        image.setAlpha(0)
+        this.scene.tweens.add({
+          targets: image,
+          duration: duration,
+          scale: 1,
+          alpha: 1,
+          ease: Phaser.Math.Easing.Quadratic.In,
+        })
+      } else {
+        image.setVisible(!!shapeId)
+        this.scene.tweens.add({
+          targets: image,
+          duration: duration,
+          y: initialSlotY + SLOT_HEIGHT * (i - 1),
+          ease: Phaser.Math.Easing.Quadratic.In,
+        })
       }
     })
   }
